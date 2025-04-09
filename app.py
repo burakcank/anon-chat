f1d846481357810f3b74418b89592468195ec901
Can Kahraman <brkcnkhrmn@gmail.com>
2025-04-09 22:22:06 +0300

    second version

--- app.py
+++ app.py
@@ -1,41 +1,64 @@
+import json
+import uuid
 from datetime import datetime, timezone
 
-from flask import Flask, jsonify
+import redis
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-from flask_sock import Sock
+from flask_socketio import SocketIO
+from loguru import logger
 
-app = Flask(__name__)
-CORS(app)
-sock = Sock(app)
+from config import Config
 
-chat_rooms = {}  # {room_id: {"messages": [], "clients": set()}}
+r = redis.Redis.from_url(Config.REDIS_URL, decode_responses=True)
+socketio = SocketIO(cors_allowed_origins="*")
 
-
-@app.route("/chat/<room_id>")
-def get_chat_messages(room_id):
-    return jsonify(chat_rooms.get(room_id, {}).get("messages", []))
+connected_clients = set[str]()
 
 
-@sock.route("/chat/<room_id>")
-def chat_socket(ws, room_id):
-    if room_id not in chat_rooms:
-        chat_rooms[room_id] = {"messages": [], "clients": set()}
+@socketio.on("connect")
+def handle_connect():
+    logger.debug(f"Client connected: {request.sid}")
+    connected_clients.add(request.sid)
+    logger.debug(f"Connected clients: {connected_clients}")
+    socketio.emit("client_count", len(connected_clients))
 
-    chat_rooms[room_id]["clients"].add(ws)
 
-    try:
-        while True:
-            print("active clients", len(chat_rooms[room_id]["clients"]))
-            message = ws.receive()
+@socketio.on("disconnect")
+def handle_disconnect():
+    logger.debug(f"Client disconnected: {request.sid}")
+    connected_clients.discard(request.sid)
+    logger.debug(f"Connected clients: {connected_clients}")
+    socketio.emit("client_count", len(connected_clients))
+
+
+@socketio.on("chat_message")
+def handle_message(data):
+    logger.debug(f"Received message: {data}")
+    message = {
+        "room_id": data["room_id"],
+        "message": data["message"],
+        "timestamp": datetime.now(timezone.utc).isoformat(),
+    }
+    message_key = f"{data['room_id']}:{uuid.uuid4()}"
+    r.set(message_key, json.dumps(message), ex=60 * 60 * 24)  # 24 hours
+    socketio.emit("chat_message", message, skip_sid=request.sid)
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    CORS(app)
+    socketio.init_app(app)
+
+    @app.route("/chat/<room_id>")
+    def chat_history(room_id):
+        messages = []
+        for key in r.scan_iter(f"{room_id}:*"):
+            message = r.get(key)
             if message:
-                chat_message = {
-                    "message": message,
-                    "sent_at": datetime.now(tz=timezone.utc).isoformat(),
-                }
-                chat_rooms[room_id]["messages"].append(chat_message)
+                messages.append(json.loads(message))
 
-                for client in chat_rooms[room_id]["clients"]:
-                    if client != ws:
-                        client.send(message)
-    finally:
-        chat_rooms[room_id]["clients"].remove(ws)
+        return jsonify(messages)
+
+    return app
